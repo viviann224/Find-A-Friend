9c06d80ee8a516744445d49b465a0ef055561ec4
viviann224 <viviann224@gmail.com>
2018-02-27 22:56:17 -0600

    updating with comments

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,6 +16,41 @@ var friendArray = [
 		      1
 		    ]
 
+	},
+	{
+		
+		"name":"Martini",
+  		"photo":"http://i63.tinypic.com/orihsg.png",
+		"scores":[
+		      2,
+		      1,
+		      4,
+		      4,
+		      3,
+		      1,
+		      5,
+		      5,
+		      2,
+		      1
+		    ]
+
+	},
+	{
+		"name":"Grumpy Cat",
+  		"photo":"https://scontent.cdninstagram.com/vp/6b3f9776e48c7eb2b7fbb8c1438ca8ed/5AEAEEB7/t51.2885-15/s640x640/sh0.08/e35/26869996_267784480423592_5727910119866368_n.jpg",
+		"scores":[
+		      1,
+		      1,
+		      4,
+		      2,
+		      5,
+		      2,
+		      2,
+		      5,
+		      2,
+		      1
+		    ]
+
 	}
 ];
 
