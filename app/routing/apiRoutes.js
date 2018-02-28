68d079816f57f7b7d3becf1785e5b8140191d2cf
viviann224 <viviann224@gmail.com>
2018-02-27 23:58:41 -0600

    cleaning up code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,17 @@
 var friendArray = require("../data/friends");
 
 module.exports =function (app)
-{	//returns data
+{	//when user uses this path go ahead and 
+	//display the data from the API
 	app.get("/api/data", function (req,res)
 	{
 		res.json(friendArray);
 	});
-
+	//when user uses this path go ahead and 
+	//add data to the API
 	app.post("/api/data", function(req, res) 
 	{
-	    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-	    // It will do this by sending out the value "true" have a table
-	    // req.body is available since we're using the body-parser middleware
-
-	      friendArray.push(req.body);
-	      res.json(true);
-	   
-	  });
-	//need to add post code for the server to respond to request
-	//if(friendArray.length>=0)
-	//friendArray.push(req.body);
-	//res.json(true);
-	//else
-	//res.json(false);
-
-
-	//clear post to drive a delete fx
+      friendArray.push(req.body);
+      res.json(true);  
+	});
 }
--- htmlRoutes.js
+++ htmlRoutes.js
@@ -1,11 +1,11 @@
 var path =require("path");
 
 module.exports = function(app) {
-	
+	//route to main page
 	app.get("/home", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
   });
-
+  //route to survey
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
