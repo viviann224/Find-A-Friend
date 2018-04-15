c4842bc0f08b306a7aeed39922408dc8b0d12270
viviann224 <viviann224@gmail.com>
2018-04-14 21:29:34 -0500

    updated and added validation with comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,4 @@
+//import default list of friends to start
 var friendArray = require("../data/friends");
 
 module.exports =function (app)
@@ -19,58 +20,45 @@ module.exports =function (app)
       friendDifference: 1000
     };
 
-    // Here we take the result of the user"s survey POST and parse it.
+    //grabbing user's post data (from survey) and parsing it.
     var userData = req.body;
-
-		//console.log(userData);
+		//to find a match use scores to determine compatiblity
     var userScores = userData.scores;
-		// Here we take the result of the user"s survey POST and parse it.
 
+    //totalDifference will be used to calculate the score of the current
+		//friend against an old friend in the list
+    var totalDifference;
 
+    //run a loop to check all friends in the list
+    for (var i = 0; i < friendArray.length; i++)
+		{
+      var currentFriend = friendArray[i];
+      totalDifference = 0;
+      //a nested for loop to compare each question from each friend
+      for (var j = 0; j < currentFriend.scores.length; j++)
+			{
+        var currentFriendScore = currentFriend.scores[j];
+        var currentUserScore = userScores[j];
 
-
-		    // This variable will calculate the difference between the user"s scores and the scores of
-		    // each user in the database
-		    var totalDifference;
-
-		    // Here we loop through all the friend possibilities in the database.
-		    for (var i = 0; i < friendArray.length; i++)
-				{
-		      var currentFriend = friendArray[i];
-		      totalDifference = 0;
-
-		      //console.log(currentFriend.name);
-
-		      // We then loop through all the scores of each friend
-		      for (var j = 0; j < currentFriend.scores.length; j++)
-					{
-		        var currentFriendScore = currentFriend.scores[j];
-		        var currentUserScore = userScores[j];
-
-		        // We calculate the difference between the scores and sum them into the totalDifference
-		        totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
-
-					}
-					console.log("total diff for "+ friendArray[i].name+ " "+totalDifference);
-					console.log(totalDifference <= bestMatch.friendDifference);
-		      // If the sum of differences is less then the differences of the current "best match"
-		      if (totalDifference <= bestMatch.friendDifference)
-					{
-		        // Reset the bestMatch to be the new friend.
-		        bestMatch.name = currentFriend.name;
-		        bestMatch.photo = currentFriend.photo;
-		        bestMatch.friendDifference = totalDifference;
-		      }
-		    }
-
-		    // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
-		    // the database will always return that the user is the user's best friend).
+        //calculate the difference in each question from the new friend
+				//compared to the current friend
+        totalDifference += Math.abs(parseInt(currentUserScore)
+				- parseInt(currentFriendScore));
+			}
+      //assume the best match is the lowest score.
+			//if the totalDifferent between the current friend and the new friend is
+			//lower, then set current friend as the best match
+      if (totalDifference <= bestMatch.friendDifference)
+			{
+        //then go ahead and set the current friend to the most compatiable friend
+        bestMatch.name = currentFriend.name;
+        bestMatch.photo = currentFriend.photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+    }
+				//once there is a match go ahead and add the newfriend to the friend list
 		    friendArray.push(userData);
-				console.log(bestMatch.name );
-				console.log(bestMatch.photo);
-				console.log(bestMatch.friendDifference) ;
-		    // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+		    //and return the bestMatch for the new friend.
 		    res.json(bestMatch);
-
 	});
 }
